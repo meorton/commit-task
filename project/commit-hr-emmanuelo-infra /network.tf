4046cdfc20058e8c9af2e023fb36964c8d52cd2d
meorton <50681038+meorton@users.noreply.github.com>
2025-02-23 07:13:16 -0500

    Update network.tf

--- project/commit-hr-emmanuelo-infra /network.tf
+++ project/commit-hr-emmanuelo-infra /network.tf
@@ -9,18 +9,18 @@ resource "google_compute_subnetwork" "commit_subnet-infra" {
   project                 = var.project_id
   private_ip_google_access   = true
   name          = "commit_subnet-infrawork"
-  ip_cidr_range = "100.120.214.0/28"#"100.120.214.24/29"
+  ip_cidr_range = "100.120.215.0/28"#"100.120.214.24/29"
   region        = "northamerica-northeast1"
   network       = google_compute_network.commit_network-infra.id
   secondary_ip_range {
     
     range_name    = "commit_subnet-infraip"
-    ip_cidr_range = "10.16.0.0/14"
+    ip_cidr_range = "10.17.0.0/14"
  }
 
      secondary_ip_range {
     range_name    = "commit_subnet-infrawork-secondaryip"
-    ip_cidr_range = "10.32.0.0/16"
+    ip_cidr_range = "10.33.0.0/16"
      }
 
   
