00e217dccd4d9ad8710dc04e48e14633923271dd
meorton <50681038+meorton@users.noreply.github.com>
2025-02-23 18:02:49 -0500

    Update artifact-repo.tf

--- project/commit-hr-emmanuelo-infra /artifact-repo.tf
+++ project/commit-hr-emmanuelo-infra /artifact-repo.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "commit-task" {
   location      = "northamerica-northeast1"
-  repository_id = "applications"
+  repository_id = "commit-task"
   description   = "commit-task-images"
   format        = "DOCKER"
   project      = var.project_id
