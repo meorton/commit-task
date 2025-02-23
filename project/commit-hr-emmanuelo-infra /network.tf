99ee1c2577d647fd400a844b818f4fa04bc2d77e
meorton <50681038+meorton@users.noreply.github.com>
2025-02-23 07:11:20 -0500

    Create network.tf

---
+++ project/commit-hr-emmanuelo-infra /network.tf
@@ -0,0 +1,27 @@
+resource "google_compute_network" "commit_network-infra" {
+  auto_create_subnetworks = false
+  project                 = var.project_id
+  name                    = "tbm-shopfront-vpc"
+}
+
+resource "google_compute_subnetwork" "commit_subnet-infra" {
+  depends_on = [google_compute_network.commit_network-infra]
+  project                 = var.project_id
+  private_ip_google_access   = true
+  name          = "commit_subnet-infrawork"
+  ip_cidr_range = "100.120.214.0/28"#"100.120.214.24/29"
+  region        = "northamerica-northeast1"
+  network       = google_compute_network.commit_network-infra.id
+  secondary_ip_range {
+    
+    range_name    = "commit_subnet-infraip"
+    ip_cidr_range = "10.16.0.0/14"
+ }
+
+     secondary_ip_range {
+    range_name    = "commit_subnet-infrawork-secondaryip"
+    ip_cidr_range = "10.32.0.0/16"
+     }
+
+  
+}
