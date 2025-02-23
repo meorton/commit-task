9dacd2e7f55fc745ca845d4ea20b7adde27cf879
meorton <50681038+meorton@users.noreply.github.com>
2025-02-23 09:14:51 -0500

    Update backend.tf

--- project/commit-hr-emmanuelo-infra /backend.tf
+++ project/commit-hr-emmanuelo-infra /backend.tf
@@ -1,8 +1,8 @@
 
 terraform {
   required_version = ">= 0.14.9" 
-  backend "gcs" {
-    bucket = "commit-it-commit-hr-emmanuelo-infra-terraform-state"
-    prefix = "env/commit-hr-emmanuelo-infra"
-  }
+  #backend "gcs" {
+    #bucket = "commit-it-commit-hr-emmanuelo-infra-terraform-state"
+    #prefix = "env/commit-hr-emmanuelo-infra"
+  #}
 }
