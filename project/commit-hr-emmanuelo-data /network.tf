0e51be00f90ad21611fba8188b8282216c11ed4d
meorton <50681038+meorton@users.noreply.github.com>
2025-02-23 08:46:26 -0500

    Update network.tf

--- project/commit-hr-emmanuelo-data /network.tf
+++ project/commit-hr-emmanuelo-data /network.tf
@@ -53,7 +53,6 @@ resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.commit-network-data.self_link
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
-  project                 = var.project_id
 
   depends_on = [google_compute_global_address.private_ip_range]
 }
