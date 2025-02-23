a4eb264c23e9fd9b5878ddf30c5cd16704c0fca7
meorton <50681038+meorton@users.noreply.github.com>
2025-02-23 07:52:25 -0500

    Update network.tf

--- project/commit-hr-emmanuelo-infra /network.tf
+++ project/commit-hr-emmanuelo-infra /network.tf
@@ -1,25 +1,25 @@
-resource "google_compute_network" "commit_network-infra" {
+resource "google_compute_network" "commit-network-infra" {
   auto_create_subnetworks = false
   project                 = var.project_id
   name                    = "tbm-shopfront-vpc"
 }
 
-resource "google_compute_subnetwork" "commit_subnet-infra" {
-  depends_on = [google_compute_network.commit_network-infra]
+resource "google_compute_subnetwork" "commit-subnet-infra" {
+  depends_on = [google_compute_network.commit-network-infra]
   project                 = var.project_id
   private_ip_google_access   = true
-  name          = "commit_subnet-infrawork"
+  name          = "commit-subnet-infrawork"
   ip_cidr_range = "100.120.215.0/28"#"100.120.214.24/29"
   region        = "northamerica-northeast1"
-  network       = google_compute_network.commit_network-infra.id
+  network       = google_compute_network.commit-network-infra.id
   secondary_ip_range {
     
-    range_name    = "commit_subnet-infraip"
+    range_name    = "commit-subnet-infraip"
     ip_cidr_range = "10.17.0.0/14"
  }
 
      secondary_ip_range {
-    range_name    = "commit_subnet-infrawork-secondaryip"
+    range_name    = "commit-subnet-infrawork-secondaryip"
     ip_cidr_range = "10.33.0.0/16"
      }
 
