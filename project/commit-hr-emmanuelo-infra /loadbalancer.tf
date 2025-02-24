1ff59488daaf819ef1c99380867bea9a41020998
meorton <50681038+meorton@users.noreply.github.com>
2025-02-24 12:00:52 -0500

    Update loadbalancer.tf

--- project/commit-hr-emmanuelo-infra /loadbalancer.tf
+++ project/commit-hr-emmanuelo-infra /loadbalancer.tf
@@ -23,6 +23,7 @@ network_endpoint_type = "SERVERLESS"
   name                    = "commit-${var.resource_name}-neg"
   network                  = "commit-network-infra"
   subnetwork               = "commit-subnet-infrawork"
+  Zone = "northamerica-northeast1"
   default_port = "8080"
 }
 
