305948719d0171a65d382dce3b805fe20385864e
meorton <50681038+meorton@users.noreply.github.com>
2025-02-24 10:52:36 -0500

    Update variables.tf

--- project/commit-hr-emmanuelo-infra /variables.tf
+++ project/commit-hr-emmanuelo-infra /variables.tf
@@ -3,3 +3,7 @@ variable "project_id" {}
 variable "project_name" {}
 variable "project_number" {}
 variable "env" {}
+# Variable for the resource name
+variable "resource_name" {
+  default = "commit-docker"
+}
