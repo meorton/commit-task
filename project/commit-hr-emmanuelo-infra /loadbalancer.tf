4f2d4a50dc47859fcf38ddb8967b42a0ea80443c
meorton <50681038+meorton@users.noreply.github.com>
2025-02-24 11:09:34 -0500

    Update loadbalancer.tf

--- project/commit-hr-emmanuelo-infra /loadbalancer.tf
+++ project/commit-hr-emmanuelo-infra /loadbalancer.tf
@@ -10,6 +10,7 @@ resource "google_compute_global_address" "commit_ip" {
 # Health Check for Backend Service (for monitoring health of Cloud Run service)
 resource "google_compute_health_check" "commit_health_check" {
   name               = "commit-${var.resource_name}-health-check"
+  project                 = var.project_id
   http_health_check {
     port = 8080
     request_path = "/"
@@ -18,6 +19,7 @@ resource "google_compute_health_check" "commit_health_check" {
 
 # Backend Service for Internal Load Balancer (using Cloud Run)
 resource "google_compute_backend_service" "commit_backend" {
+  project                 = var.project_id
   name                  = "commit-${var.resource_name}-backend"
   protocol              = "HTTP"
   load_balancing_scheme = "EXTERNAL"
@@ -38,12 +40,14 @@ resource "google_compute_url_map" "commit_url_map" {
 
 # HTTP Proxy (to handle HTTP requests)
 resource "google_compute_target_http_proxy" "commit_http_proxy" {
+  project                 = var.project_id
   name    = "commit-${var.resource_name}-http-proxy"
   url_map = google_compute_url_map.commit_url_map.id
 }
 
 # Global Forwarding Rule (to forward traffic to Cloud Run service)
 resource "google_compute_global_forwarding_rule" "commit_forwarding_rule" {
+  project                 = var.project_id
   name       = "commit-${var.resource_name}-forwarding-rule"
   ip_address = google_compute_global_address.commit_ip.address
   target     = google_compute_target_http_proxy.commit_http_proxy.id
